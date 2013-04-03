38cb397e4a4dba5be0041c2c6aa14148a43d9d59
Pietro Martinelli <amicofragile@gmail.com>
2013-04-03 21:47:55 +0200

    Serializable classes should have serialVersionUid static field

--- src/main/java/com/blogspot/javapeanuts/jarachis/webutils/CauseAwareServletException.java
+++ src/main/java/com/blogspot/javapeanuts/jarachis/webutils/CauseAwareServletException.java
@@ -3,6 +3,8 @@ package com.blogspot.javapeanuts.jarachis.webutils;
 import javax.servlet.ServletException;
 
 public class CauseAwareServletException extends ServletException {
+	private static final long serialVersionUID = 1L;
+
 	public CauseAwareServletException(String message, Throwable cause) {
 		super(message, cause);
 	}
